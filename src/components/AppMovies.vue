4b930f86289c1dc886dfbda40ccaf4ef3947334c
nicomanunta <nic.manun@gmail.com>
2024-01-15 18:37:03 +0100

    aggiunta trama in AppMovies, sistemato il layout

--- src/components/AppMovies.vue
+++ src/components/AppMovies.vue
@@ -66,13 +66,17 @@ export default {
             <div class="row">
                 <div class="col-4 colonna-tv" v-for="film, index in store.films" :key="index">
                     <div class="row d-flex flex-column">
-                        <div class="col-12 poster-container">
+                        <div class="col-12 poster-container p-0  ">
                             <div class="front-poster">
                                 <img class="img-w" :src="getPoster(film.poster_path)" alt="">
                             </div>
-                            <div class="back-poster">
-                                <h3>{{film.title}}</h3>
-                                <h4>{{film.original_title}}</h4>
+                            <div class="back-poster d-flex flex-column align-items-center">
+                                <h5 class="pt-3">Titolo</h5>
+                                <h5 class="mb-4 px-2 "><b>{{film.title}}</b></h5>
+                                <h6>Titolo originale</h6>
+                                <h6 class="mb-4 px-2"><b>{{film.original_title}}</b></h6>
+                                <h5>Trama</h5>
+                                <span class="text-center mb-5">{{film.overview}}</span>
                                 <div>
                                     <i v-for="star in getStar(film.vote_average).stelle_piene" :key="index" class="fa-star fa-solid"></i>
                                     <i v-for="star in getStar(film.vote_average).stelle_vuote" :key="index" class="fa-star fa-regular"></i>  
@@ -97,6 +101,7 @@ h2 {
 
 .img-w {
     width: 100%;
+    height: 660px;
 }
 
 .poster-container {
@@ -105,17 +110,21 @@ h2 {
     transform-style: preserve-3d;
     transition: transform 1s;
     display: flex;
-
+    color: white;
 }
 
 .back-poster {
     position: absolute;
     width: 100%;
     height: 100%;
+    transform: rotateY(180deg);
+    background-color: black;
+    overflow: scroll;
 }
 
 .front-poster {
     position: relative;
+    transform: rotateY(0deg);
 }
 
 .front-poster,
@@ -123,15 +132,6 @@ h2 {
     flex: 1;
     backface-visibility: hidden;
     z-index: 2;
-}
-
-.front-poster {
-    transform: rotateY(0deg);
-}
-
-.back-poster {
-    transform: rotateY(180deg);
-    background-color: #fff;
 }
 
 .colonna-tv:hover .poster-container {
